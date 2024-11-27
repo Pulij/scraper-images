9b0009d305af3ebc8cbcb680b9f9551aa44cba8e
Артём И. <99142900+Pulij@users.noreply.github.com>
2024-11-27 20:58:52 +0300

    Update google-image.ts

--- src/google-image.ts
+++ src/google-image.ts
@@ -1,20 +1,17 @@
-import * as cheerio from 'cheerio'
-import got from 'got'
-import { GoogleImage, GoogleImageSchema } from '../types/index.js'
-import { DEFAULT_HEADERS } from './constant.js'
+import got from 'got';
+import { GoogleImage, GoogleImageSchema } from '../types/index.js';
+import { DEFAULT_HEADERS } from './constant.js';
 
-export default async function googleImage (query: string): Promise<GoogleImage[]> {
+export default async function googleImage(query: string): Promise<GoogleImage[]> {
     const data = await got(`https://www.google.com/search?q=${query}&tbm=isch`, {
-        headers: DEFAULT_HEADERS
-    }).text()
-
-    const $ = cheerio.load(data)
+        headers: DEFAULT_HEADERS,
+    }).text();
     const pattern =
-        /\[1,\[0,"(?<id>[\d\w\-_]+)",\["https?:\/\/(?:[^"]+)",\d+,\d+\]\s?,\["(?<url>https?:\/\/(?:[^"]+))",\d+,\d+\]/gm
-    const matches = $.html().matchAll(pattern)
-    const decodeUrl = (url: string) => decodeURIComponent(JSON.parse(`"${url}"`))
+        /\[1,\[0,"[\d\w\-_]+",\["https?:\/\/(?:[^"]+)",\d+,\d+\]\s?,\["(?<url>https?:\/\/(?:[^"]+))",\d+,\d+\]/gm;
+    const decodeUrl = (url: string) => decodeURIComponent(JSON.parse(`"${url}"`));
+    const matches = data.matchAll(pattern);
     return [...matches]
         .map(({ groups }) => decodeUrl(groups?.url as string))
         .filter((v) => /.*\.jpe?g|png$/gi.test(v))
-        .map((value) => GoogleImageSchema.parse(value))
+        .map((value) => GoogleImageSchema.parse(value));
 }
